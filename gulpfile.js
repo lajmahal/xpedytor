4429842e015f73f40e9abd1bfd2bc69ecf14bc4e
lajmahal <laj.lal@outlook.com>
2016-03-19 18:08:12 +0800

    Works with jshint and jscs -- fails on purpose, to be fixed in next commit

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,23 @@
 
 var gulp = require('gulp');
 
+// gulp plugins
+var jscs = require('gulp-jscs');
+var jshint = require('gulp-jshint');
+
+// Hello World task
 gulp.task('hello-world', function () {
     console.log('Hello World!');
+});
+
+// vet task
+gulp.task('vet', function () {
+
+    return gulp
+        .src('./xpedytor-site/src/js/**/*.js')
+        .pipe(jscs())
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish', {verbose: true}))
+        .pipe(jshint.reporter('fail'));
+
 });
